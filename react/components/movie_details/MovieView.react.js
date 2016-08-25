ddab94fbf75403153107b8d6ec6a9a7babbe0364
Kate Odnous <kate.odnous@airbnb.com>
2016-08-25 08:29:30 -0700

    Use different layout for  detail movie view in the landscape mode

--- react/components/movie_details/MovieView.react.js
+++ react/components/movie_details/MovieView.react.js
@@ -10,9 +10,15 @@ import MovieShape from '../../shapes/MovieShape';
 import monent from 'moment';
 import MovieRatingStars from '../shared/MovieRatingStars.react';
 
+const ORIENTATION_PORTRAIT = 'portrait';
+const ORIENTATION_LANDSCAPE = 'landscape';
 const styles = StyleSheet.create({
   posterImage: {
     height: 200,
+  },
+  posterImageLandscape: {
+    height: 150,
+    flex: 1,
   },
   movieTitle: {
     fontSize: 16,
@@ -30,26 +36,68 @@ const styles = StyleSheet.create({
   ratingView: {
     marginTop: 8,
   },
+  row: {
+    flexDirection: 'row',
+  },
+  column: {
+    flex: 1,
+  },
 });
 
 class MovieView extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orientation: ORIENTATION_PORTRAIT,
+    };
+
+    this.onLayout = this.onLayout.bind(this);
+  }
+  onLayout({ nativeEvent }) {
+    const { layout } = nativeEvent;
+    const { width, height } = layout;
+    let newOrientation = ORIENTATION_PORTRAIT;
+    if (width > height) {
+      newOrientation = ORIENTATION_LANDSCAPE;
+    }
+
+    if (newOrientation !== this.state.orientation) {
+      this.setState({
+        orientation: newOrientation,
+      })
+    }
+  }
+  render() {
     const { movie } = this.props;
+    const { orientation } = this.state;
     return (
-      <ScrollView>
-        <Image
-          style={styles.posterImage}
-          source={{uri: movie.backdrop_path}}
-          resizeMode="cover"
-        />
+      <ScrollView onLayout={this.onLayout}>
+        {orientation === ORIENTATION_PORTRAIT &&
+          <Image
+            style={styles.posterImage}
+            source={{uri: movie.backdrop_path}}
+            resizeMode="cover"
+          />
+        }
         <View style={styles.movieInfo}>
-          <Text style={styles.movieTitle}>
-            {movie.title}
-          </Text>
-          <Text style={styles.text}>
-            Released {monent(movie.release_date).format('MMM YYYY')}
-          </Text>
-          <MovieRatingStars movie={movie} style={styles.ratingView} />
+          <View style={styles.row}>
+            <View style={styles.column}>
+              <Text style={styles.movieTitle}>
+                {movie.title}
+              </Text>
+              <Text style={styles.text}>
+                Released {monent(movie.release_date).format('MMM YYYY')}
+              </Text>
+              <MovieRatingStars movie={movie} style={styles.ratingView} />
+            </View>
+            {orientation === ORIENTATION_LANDSCAPE &&
+              <Image
+                style={styles.posterImageLandscape}
+                source={{ uri: movie.backdrop_path }}
+              />
+            }
+          </View>
           <Text style={styles.text}>
             {movie.overview}
           </Text>
